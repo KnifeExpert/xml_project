9bf29cbd1be2ae21f61ffe2121aaca302b107cd0
KnifeExpert <larsowina@gmail.com>
2024-05-31 20:57:00 +0200

    Force-updated modified feeds

---
+++ upload_to_github.bat
@@ -0,0 +1,26 @@
+@echo off
+set LOGFILE=C:\Users\larso\xml_project\upload_to_github.log
+echo Task started at %date% %time% > %LOGFILE%
+
+echo Running download_xml.py for original supplier >> %LOGFILE% 2>&1
+python C:\Users\larso\xml_project\download_xml.py >> %LOGFILE% 2>&1
+
+echo Running modify_xml.py for original supplier >> %LOGFILE% 2>&1
+python C:\Users\larso\xml_project\modify_xml.py >> %LOGFILE% 2>&1
+
+echo Running modify_xml_rosler.py for Rosler supplier >> %LOGFILE% 2>&1
+python C:\Users\larso\xml_project\modify_xml_rosler.py >> %LOGFILE% 2>&1
+
+echo Running modify_xml_opinel.py for Opinel supplier >> %LOGFILE% 2>&1
+python C:\Users\larso\xml_project\modify_xml_opinel.py >> %LOGFILE% 2>&1
+
+echo Running git commands >> %LOGFILE% 2>&1
+cd C:\Users\larso\xml_project
+
+git add -A >> %LOGFILE% 2>&1
+
+git commit -m "Force-updated modified feeds" >> %LOGFILE% 2>&1
+git push origin master >> %LOGFILE% 2>&1
+
+echo Task finished at %date% %time% >> %LOGFILE%
+
