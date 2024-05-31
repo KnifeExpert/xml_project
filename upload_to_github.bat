6fc8e0bb6e62751c19deb7f7646152e8fe758057
KnifeExpert <larsowina@gmail.com>
2024-05-31 17:29:58 +0200

    Updated modified feeds for all suppliers

--- upload_to_github.bat
+++ upload_to_github.bat
@@ -23,16 +23,15 @@ python C:\Users\larso\xml_project\modify_xml_opinel.py >> %LOGFILE% 2>&1
 echo Running git commands >> %LOGFILE% 2>&1
 cd C:\Users\larso\xml_project
 
-echo Current directory: >> %LOGFILE% 2>&1
-cd >> %LOGFILE% 2>&1
+echo Explicitly removing files from git >> %LOGFILE% 2>&1
+git rm --cached modified_supplier.xml >> %LOGFILE% 2>&1
+git rm --cached modified_rosler_supplier.xml >> %LOGFILE% 2>&1
+git rm --cached modified_opinel_supplier.xml >> %LOGFILE% 2>&1
 
-echo Listing files: >> %LOGFILE% 2>&1
-dir >> %LOGFILE% 2>&1
+echo Adding all files to git >> %LOGFILE% 2>&1
+git add --all >> %LOGFILE% 2>&1
 
-echo Force-adding files to git >> %LOGFILE% 2>&1
-git add -A >> %LOGFILE% 2>&1
-
-echo Checking status: >> %LOGFILE% 2>&1
+echo Checking status >> %LOGFILE% 2>&1
 git status >> %LOGFILE% 2>&1
 
 echo Committing changes >> %LOGFILE% 2>&1
@@ -42,6 +41,9 @@ echo Pushing changes to GitHub >> %LOGFILE% 2>&1
 git push origin master >> %LOGFILE% 2>&1
 
 echo Task finished at %date% %time% >> %LOGFILE%
+
+
+
 
 
 
