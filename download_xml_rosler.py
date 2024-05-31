c2556f218c2e71762e125fa529eb698c8cebfb5d
KnifeExpert <larsowina@gmail.com>
2024-05-31 21:04:00 +0200

    Removed unnecessary files from repository

--- download_xml_rosler.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# URL od dodávateľa
-url = 'https://vreckovynoz.sk/heureka'
-response = requests.get(url)
-with open('C:\\Users\\larso\\xml_project\\rosler_supplier.xml', 'wb') as file:
-    file.write(response.content)
